c3665952ced7f1457508e0ae54095fbcc9a70a54
Apoliana Lívia <159330898+apolianalivia@users.noreply.github.com>
2024-10-28 16:20:48 -0300

    Add files via upload

---
+++ CalculadoraJuros/js/javaS.js
@@ -0,0 +1,9 @@
+document.getElementById('calcular').addEventListener('click',function(){
+    const value = document.getElementById('value').value;
+    const free = (document.getElementById('free').value) / 100;
+    const time = document.getElementById('time').value;
+
+    const total = value * (1 + free)**time;
+
+    document.getElementById('total').innerHTML = ("R$" + total.toFixed(2));
+});
